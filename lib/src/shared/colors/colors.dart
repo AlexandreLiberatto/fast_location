2cddd57141ec2fa1100b1809343be7db19cb2b15
Alexandre liberato <alexandreliberatto@gmail.com>
2024-09-24 08:44:04 -0300

    Criando arquivo colors.dart e as cores que usaremos
    Signed-off-by: Alexandre liberato <alexandreliberatto@gmail.com>

---
+++ lib/src/shared/colors/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Colors.blue;
+  static const Color secondary = Colors.blueAccent;
+  static const Color background = Colors.white;
+  static const Color text = Colors.black;
+  // Adicione mais cores conforme necessário
+}
